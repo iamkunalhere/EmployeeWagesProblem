c52b197504140a66751b841de0e7e21d420ff9ae
kunal jadhav <iamkunalhere@gmail.com>
2020-03-04 18:15:11 +0530

    Added: Daily Wage Calculation

--- EmpWageComputaion.sh
+++ EmpWageComputaion.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
 #Welcome to Employee Wage Computation Program
 
+function Attendence() {
 state=$((RANDOM%2))
 if [ $state == 1 ]
 then
 	echo employee is present
+	DailyWage
 else
 	echo employee is absent
 fi
+}
+function DailyWage() {
+	EmpRatePerHour=20
+	WorkingHours=8
+	Salary=$(($EmpRatePerHour * $WorkingHours))
+	echo $Salary
+}
+Attendence
