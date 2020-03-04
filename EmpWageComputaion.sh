9550e55a46f50ea01201032887bfbe1d49ecb15a
kunal jadhav <iamkunalhere@gmail.com>
2020-03-04 19:30:46 +0530

    Added: Wage Calculation till 100 hours or working days are above 20

--- EmpWageComputaion.sh
+++ EmpWageComputaion.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -x
 #Welcome to Employee Wage Computation Program
 
-EmpRatePerHour=20
-Numberofdays=20
-for ((day=1; day<=Numberofdays; day++))
+EmpRatePerHour=30
+TotalWorkingDays=20
+MaxHoursInMonth=100
+hours=0
+days=0
+TotalWorkHours=0
+while [[ days -lt TotalWorkingDays && hours -le MaxHoursInMonth ]]
 do
+	((days++))
 	state=$((RANDOM%3))
 	case $state in
 		"1")
@@ -17,6 +22,9 @@ do
 		EmpHours=0
 		;;
 	esac
-	salary=$(($salary+$(($EmpRatePerHour*$EmpHours))))
+	echo "day $days employee works $EmpHours hours"
+	salary=$(($salary+$EmpHours*$EmpRatePerHour))
+	TotalWorkHours=$(($TotalWorkHours+$EmpHours))
 done
+echo "Employee works for $TotalWorkHours hours in 20 days"
 echo "$salary is salary of employee in a month"
