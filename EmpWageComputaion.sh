2856f72d1e00f8b6a132addd89e39e23e9d9e495
kunal jadhav <iamkunalhere@gmail.com>
2020-03-05 11:35:16 +0530

    Added: Function for Calc EmpHours

--- EmpWageComputaion.sh
+++ EmpWageComputaion.sh
@@ -1,30 +1,47 @@
 #!/bin/bash -x
 #Welcome to Employee Wage Computation Program
 
+function getEmpHoursFun() {
+case $1 in
+   "1")
+   EmpHours=8
+   ;;
+   "2")
+   EmpHours=4
+   ;;
+   *)
+	EmpHours=0
+   ;;
+esac
+echo $EmpHours
+}
+
 EmpRatePerHour=30
 TotalWorkingDays=20
 MaxHoursInMonth=100
 hours=0
 days=0
 TotalWorkHours=0
+
 while [[ days -lt TotalWorkingDays && hours -le MaxHoursInMonth ]]
 do
 	((days++))
-	state=$((RANDOM%3))
-	case $state in
-		"1")
-		EmpHours=8
-		;;
-		"2")
-		EmpHours=4
-		;;
-		*)
-		EmpHours=0
-		;;
-	esac
-	echo "day $days employee works $EmpHours hours"
+	echo "------------------------------Day $days"
+	EmpHours="$( getEmpHoursFun $((RANDOM%3)) )"
+	if [[ $EmpHours -eq 0 ]]
+	then
+		echo "Employee is Absent"
+	elif [[ $EmpHours -eq 8 ]]
+	then
+		echo "Employee is FullTime"
+	else
+		echo "Employee is PartTime"
+	fi
+	echo "Employee works for $EmpHours hours"
+	echo "-----------------------------------"
 	salary=$(($salary+$EmpHours*$EmpRatePerHour))
 	TotalWorkHours=$(($TotalWorkHours+$EmpHours))
 done
+
 echo "Employee works for $TotalWorkHours hours in 20 days"
 echo "$salary is salary of employee in a month"
